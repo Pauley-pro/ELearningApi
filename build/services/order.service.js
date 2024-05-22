98cdeeb1671315364d967133f68a2aadb482a44e
Paul Adelaja <pauladelaja54@gmail.com>
2024-05-22 19:23:49 +0100

    uploading backend folder

---
+++ build/services/order.service.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAllOrdersService = exports.newOrder = void 0;
+const catchAsyncError_1 = require("../middleware/catchAsyncError");
+const order_model_1 = __importDefault(require("../models/order.model"));
+// create new order
+/*export const newOrder = CatchAsyncError(async (data: any, res: Response, next: NextFunction) => {
+    const order = await OrderModel.create(data);
+    res.status(201).json({
+        success: true,
+        order,
+    });
+}); */
+// create new order
+exports.newOrder = (0, catchAsyncError_1.CatchAsyncError)(async (data, res) => {
+    const order = await order_model_1.default.create(data);
+    res.status(201).json({
+        success: true,
+        order,
+    });
+});
+// Get all orders
+const getAllOrdersService = async (res) => {
+    const orders = await order_model_1.default.find().sort({ createdAt: -1 });
+    res.status(201).json({
+        success: true,
+        orders,
+    });
+};
+exports.getAllOrdersService = getAllOrdersService;
